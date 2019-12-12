2b756b72b4e912271215b422c72148582338be59
Austin Johnson <technicallyaustin@gmail.com>
2019-12-12 18:41:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  arr = [1,2,3]
+  arr = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -12,7 +12,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.shift(element)
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
@@ -20,7 +20,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array.unshift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
